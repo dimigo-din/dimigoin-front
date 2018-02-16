9ffe45c82288a186217e5cfc1f11ef2d7d2be9ac
a1p4ca <dreamaker7770@gmail.com>
2018-02-16 20:14:32 +0900

    Better error handling.

--- app/store/modules/account/index.js
+++ app/store/modules/account/index.js
@@ -1,5 +1,4 @@
 import { getAccessToken } from '../../../src/api'
-import {Status} from '../../../src/util'
 
 const LOGIN_REQUEST = 'ACCOUNT:LOGIN_REQUEST'
 const LOGIN_SUCCESS = 'ACCOUNT:LOGIN_SUCCESS'
@@ -14,7 +13,6 @@ export default {
     email: '',
     photoUrl: '',
     pending: false,
-    status: Status(true),
     isLoggedIn: !!window.localStorage.getItem('token')
   },
 
@@ -26,11 +24,6 @@ export default {
     [LOGIN_SUCCESS] (state, payload) {
       state.isLoggedIn = true
       state.pending = false
-      state.status = Status(true)
-    },
-
-    [LOGIN_FAIL] (state, payload) {
-      state.status = Status(false, payload)
     },
 
     [LOGOUT] (state) {
@@ -41,19 +34,22 @@ export default {
   actions: {
     async login ({ commit }, { id, password }) {
       commit(LOGIN_REQUEST)
-      try {
-        const token = await getAccessToken(id, password)
-        if (token) {
-          window.localStorage.setItem('token', token)
-          commit(LOGIN_SUCCESS, { token })
-        } else {
-          const err = new Error()
-          err.name = 'NoTokenError'
-          throw err
-        }
-      } catch (error) {
-        commit(LOGIN_FAIL, { error })
+      const token = await getAccessToken(id, password)
+
+      /**
+       * getAccessToken throws errors whether logging in is successful, or not.
+       * If getAccessToken doesn't return any token, error handling is failed in getAccessToken.
+       * Therefore, if this statement is committed, bug must be there.
+       */
+      if (!token) {
+        const err = new Error()
+        err.name = 'NoTokenError'
+        err.message = '알 수 없는 이유로 로그인에 실패했습니다. 관리자에게 문의해주세요.'
+        throw err
       }
+
+      window.localStorage.setItem('token', token)
+      commit(LOGIN_SUCCESS, { token })
     },
 
     logout ({ commit }) {
