c6088626594db7dfc7002ef29a071e59e4f4b802
a1p4ca <dreamaker7770@gmail.com>
2018-03-03 02:37:41 +0900

    Insert space between bracket

--- app/src/util/index.js
+++ app/src/util/index.js
@@ -1,3 +1,3 @@
-export {default as debounce} from './debounce'
-export {default as sleep} from './sleep'
-export {default as withPrefix} from './path-with-prefix'
+export { default as debounce } from './debounce'
+export { default as sleep } from './sleep'
+export { default as withPrefix } from './path-with-prefix'
