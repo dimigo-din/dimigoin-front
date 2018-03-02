8bb4188a78822020813d622d7c89b92cd1d8c38f
a1p4ca <dreamaker7770@gmail.com>
2018-03-02 15:33:17 +0900

    Implement Divider

---
+++ app/components/DimiDivider.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'DimiDivider',
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false
+    },
+    horizontal: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    computedClass () {
+      if (this.horizontal || this.vertical === this.horizontal) {
+        return ['divider', 'divider--horizontal']
+      } else {
+        return ['divider', 'divider--vertical']
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <span :class="computedClass"/>
+</template>
+
+<style lang="scss" scoped>
+.divider {
+  position: absolute;
+  &--horizontal {
+    right: 0;
+    left: 0;
+    border-top: solid 1px $gray-lighten;
+  }
+  &--vertical {
+    top: 0;
+    bottom: 0;
+    border-left: solid 1px $gray-lighten;
+  }
+}
+</style>
