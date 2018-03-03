00053e5be6db7cff26a7dc7268673e8884232937
a1p4ca <dreamaker7770@gmail.com>
2018-03-04 03:51:06 +0900

    :tada: Implement button radio button

---
+++ app/components/DimiButtonRadio.vue
@@ -0,0 +1,49 @@
+<script>
+import DimiButton from './DimiButton.vue'
+
+export default {
+  name: 'DimiButtonRadio',
+
+  components: { DimiButton },
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: String,
+      default: ''
+    },
+    small: {
+      type: Boolean,
+      default: false
+    },
+    large: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    click () {
+      this.$emit('input', this.name)
+    }
+  }
+}
+</script>
+
+<template>
+  <dimi-button
+    :small="small"
+    :large="large"
+    :gray="name !== value"
+    @click="click"
+  >
+    <slot/>
+  </dimi-button>
+</template>
+
+<style lang="scss">
+
+</style>
