18094a00d313be312d2292119fb0991ba527e5c9
a1p4ca <dreamaker7770@gmail.com>
2018-03-07 02:41:18 +0900

    Implement base structure

---
+++ app/components/CircleCard.vue
@@ -0,0 +1,48 @@
+<script>
+import DimiCard from './DimiCard.vue'
+
+export default {
+  name: 'CircleCard',
+
+  components: { DimiCard },
+
+  props: {
+    circle: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+
+  computed: {
+    hasBadge () {
+      return true // TODO
+    }
+  }
+}
+</script>
+
+<template>
+  <dimi-card class="circle-card">
+    <div class="circle-card__info"></div>
+    <div class="circle-card__description"></div>
+    <div
+      v-if="hasBadge"
+      class="circle-card__badge"></div>
+  </dimi-card>
+</template>
+
+<style lang="scss">
+.circle-card {
+  &__info {
+    display: flex;
+  }
+
+  &__description {
+
+  }
+
+  &__badge {
+
+  }
+}
+</style>
--- app/router/paths/request/RequestCircle.vue
+++ app/router/paths/request/RequestCircle.vue
@@ -1,18 +1,25 @@
 <script>
 import ContentWrapper from '../../partial/ContentWrapper.vue'
-import ClubApplicantRow from '../../../components/ClubApplicantRow.vue'
+import CircleCard from '../../../components/CircleCard.vue'
 
 import * as circle from '../../../src/api/circle'
 
 export default {
   name: 'RequestCircle',
 
-  components: { ContentWrapper, ClubApplicantRow },
+  components: { ContentWrapper, CircleCard },
 
   data () {
     return {
       pending: false,
       circles: []
+    }
+  },
+
+  computed: {
+    appliedCircles () {
+      // TODO
+      return []
     }
   },
 
@@ -34,9 +41,23 @@ export default {
       slot="main">
       <section class="circle__section">
         <h2 class="circle__title">신청한 동아리</h2>
+        <div class="circle__circles">
+          <circle-card
+            v-for="(circle, index) in appliedCircles"
+            :key="`applied-circle-${index}`"
+            :circle="circle"
+          />
+        </div>
       </section>
       <section class="circle__section">
         <h2 class="circle__title">전체 동아리</h2>
+        <div class="circle__circles">
+          <circle-card
+            v-for="(circle, index) in circles"
+            :key="`circle-${index}`"
+            :circle="circle"
+          />
+        </div>
       </section>
     </div>
   </content-wrapper>
@@ -49,6 +70,14 @@ export default {
   }
 
   &__title {
+
+  }
+
+  &__circles {
+    display: grid;
+  }
+
+  &__circle {
 
   }
 }
