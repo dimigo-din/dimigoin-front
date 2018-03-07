609bfcb09294d060b202affdb9a35d51f0c37289
a1p4ca <dreamaker7770@gmail.com>
2018-03-08 01:01:06 +0900

    WIP : Footer

--- app/components/DimiFooter.vue
+++ app/components/DimiFooter.vue
@@ -7,17 +7,11 @@ export default {
 <template>
   <div class="footer">
     <div class="container">
-      <div class="footer__wrapper">
-        <p class="footer__copyright">© 2018 Team DIMIGOIN</p>
+      <div class="footer__container">
+        <span class="footer__copyright"><slot name="copyright"/></span>
         <div class="footer__contact">
-          <a
-            class="footer__mail"
-            href="mailto:contact@dimigo.in"
-          >contact@dimigo.in</a>
-          <a
-            class="footer__facebook"
-            href="https://fb.com/dimigoin"
-          >fb.com/dimigoin</a>
+          <span class="footer__mail"><slot name="mail"/></span>
+          <span class="footer__facebook"><slot name="facebook"/></span>
         </div>
       </div>
     </div>
@@ -28,13 +22,21 @@ export default {
 .footer {
   height: 8rem;
 
-  &__wrapper {
+  &__container {
     align-items: center;
     display: flex;
     justify-content: space-between;
   }
 
   &__copyright {
+
+  }
+
+  &__mail {
+
+  }
+
+  &__facebook {
 
   }
 }
---
+++ app/router/partial/AwesomeFooter.vue
@@ -0,0 +1,24 @@
+<script>
+import DimiFooter from '../../components/DimiFooter.vue'
+
+export default {
+  name: 'AwesomeFooter',
+  components: { DimiFooter }
+}
+</script>
+
+<template>
+  <footer>
+    <dimi-footer>
+      <span slot="copyright">© 2018 Team DIMIGOIN</span>
+      <span slot="mail"><span class="icon-email"/>contact@dimigo.in</span>
+      <a
+        slot="facebook"
+        href="https://fb.com/dimigo.in"><span class="icon-facebook"/>fb.com/dimigo.in</a>
+    </dimi-footer>
+  </footer>
+</template>
+
+<style lang="scss">
+
+</style>
