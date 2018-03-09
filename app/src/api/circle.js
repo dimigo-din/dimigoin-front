f562585000e76839db3f2d0bb54585ededc4c01f
Chalk <chalkpe@gmail.com>
2018-03-09 16:08:56 +0900

    move function up

--- app/src/api/circle.js
+++ app/src/api/circle.js
@@ -16,6 +16,25 @@ export async function getCircles () {
   }
 }
 
+export async function getAppliedCircles () {
+  try {
+    const res = await axios.get(`/circles/apply/user`)
+    return res.data['circle_user_apply']
+  } catch ({ message, response: res }) {
+    console.error(message)
+    switch (res.status) {
+      case 403:
+        throw new Error('동아리 신청 기간이 아닙니다.')
+      case 404:
+        throw new Error('존재하지 않는 동아리입니다.')
+      case 409:
+        throw new Error('신청한 동아리가 없습니다.')
+      default:
+        throw new Error('알 수 없는 오류로 잠시 후 다시 시도해주세요.')
+    }
+  }
+}
+
 export async function getCircleApplicant (circleIdx) {
   try {
     const res = await axios.get(`/circles/apply/${circleIdx}`)
@@ -43,25 +62,6 @@ export async function applyCircle (circleIdx) {
         throw new Error('존재하지 않는 동아리입니다.')
       case 409:
         throw new Error('이미 신청했습니다.')
-      default:
-        throw new Error('알 수 없는 오류로 잠시 후 다시 시도해주세요.')
-    }
-  }
-}
-
-export async function getAppliedCircles () {
-  try {
-    const res = await axios.get(`/circles/apply/user`)
-    return res.data['circle_user_apply']
-  } catch ({ message, response: res }) {
-    console.error(message)
-    switch (res.status) {
-      case 403:
-        throw new Error('동아리 신청 기간이 아닙니다.')
-      case 404:
-        throw new Error('존재하지 않는 동아리입니다.')
-      case 409:
-        throw new Error('신청한 동아리가 없습니다.')
       default:
         throw new Error('알 수 없는 오류로 잠시 후 다시 시도해주세요.')
     }
