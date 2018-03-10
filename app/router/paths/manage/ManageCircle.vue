580a70924e46049ff39064488a15391bdc4956f3
a1p4ca <dreamaker7770@gmail.com>
2018-03-11 00:45:43 +0900

    Implement router for circle management

---
+++ app/router/paths/manage/ManageCircle.vue
@@ -0,0 +1,29 @@
+<script>
+import ContentWrapper from '../../partial/ContentWrapper.vue'
+import DefaultNavbar from '../../partial/DefaultNavbar.vue'
+import DimiCard from '../../../components/DimiCard.vue'
+
+export default {
+  name: 'ManageCircle',
+
+  components: { ContentWrapper, DefaultNavbar, DimiCard }
+}
+</script>
+
+<template>
+  <content-wrapper class="mng-circle">
+    <h1 slot="header"><span class="icon-club"/>동아리 신청자 관리</h1>
+    <dimi-card slot="main">
+      <h2 class="mng-circle__title">Coin 신청자 관리</h2>
+    </dimi-card>
+  </content-wrapper>
+</template>
+
+<style lang="scss" scoped>
+.mng-circle {
+  &__title {
+    font-size: 24px;
+    color: $gray-dark;
+  }
+}
+</style>
