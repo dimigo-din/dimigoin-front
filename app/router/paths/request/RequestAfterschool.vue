1b52d7de489cc7fe14ae481d6a6484a6b1e17399
Chalk <chalkpe@gmail.com>
2018-03-12 22:05:54 +0900

    implement reCAPTCHA

--- app/router/paths/request/RequestAfterschool.vue
+++ app/router/paths/request/RequestAfterschool.vue
@@ -1,18 +1,21 @@
 <script>
+import VueRecaptcha from 'vue-recaptcha'
+
 import ContentWrapper from '../../partial/ContentWrapper.vue'
 import DimiCard from '../../../components/DimiCard.vue'
 import DimiTab from '../../../components/DimiTab.vue'
 
 import config from '../../../../config'
-const { days } = config
+const { days, sitekey } = config
 
 export default {
   name: 'RequestAfterschool',
-
-  components: { ContentWrapper, DimiCard, DimiTab },
+  components: { VueRecaptcha, ContentWrapper, DimiCard, DimiTab },
 
   data () {
     return {
+      captchaResponse: null,
+
       list: [
         [
           {
@@ -37,9 +40,8 @@ export default {
   },
 
   computed: {
-    days () {
-      return days
-    }
+    days () { return days },
+    sitekey () { return sitekey }
   },
 
   async created () {
@@ -49,6 +51,10 @@ export default {
   methods: {
     async refresh () {
 
+    },
+
+    verifyRecaptcha (response) {
+      this.captchaResponse = response
     }
   }
 }
@@ -62,7 +68,24 @@ export default {
         class="req-afsc__refresh"
         @click="refresh">새로고침</span>
     </h1>
+
     <dimi-card
+      v-if="captchaResponse === null"
+      slot="main">
+
+      <p class="req-afsc__captcha">
+        방과후 활동 신청은 정정당당한 사람의 손으로만 할 수 있습니다.<br>
+        더럽고 비열한 매크로들은 <a href="https://www.google.com/recaptcha">reCAPTCHA&trade;</a>의 힘에 쓰러질 것입니다.
+      </p>
+
+      <vue-recaptcha
+        :sitekey="sitekey"
+        @verify="verifyRecaptcha"
+        @expired="verifyRecaptcha(null)"/>
+    </dimi-card>
+
+    <dimi-card
+      v-else
       slot="main"
       class="req-afsc__main">
       <dimi-tab
@@ -96,6 +119,13 @@ export default {
 
 <style lang="scss" scoped>
 .req-afsc {
+  &__captcha {
+    color: $gray-dark;
+    font-size: 16px;
+    line-height: 1.8;
+    padding-bottom: 2em;
+  }
+
   &__main {
     padding-top: 0;
   }
