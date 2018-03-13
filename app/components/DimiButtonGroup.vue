d13a5c36c8b0372192ad2ace839bc5f80df1bb3d
a1p4ca <dreamaker7770@gmail.com>
2018-03-13 23:15:27 +0900

    Merge

---
+++ app/components/DimiButtonGroup.vue
@@ -0,0 +1,134 @@
+<template>
+  <div :class="classes">
+    <div
+      v-for="(item, index) in items"
+      :key="`button-${index}`"
+      :active="index === value"
+      class="button-group__button"
+      @click="onClick(index)">
+
+      {{ item }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+
+    items: {
+      type: Array,
+      required: true
+    },
+
+    colors: {
+      type: Array,
+      default: () => ['gray']
+    }
+  },
+
+  computed: {
+    color () {
+      return this.colors[this.value]
+    },
+
+    classes () {
+      return [
+        'button-group',
+        `button-group--${this.color}`
+      ]
+    }
+  },
+
+  methods: {
+    onClick (index) {
+      const event = {
+        vm: this,
+        value: index,
+        items: this.items,
+        prevent: false,
+        done: function () {
+          this.prevent ? null : this.vm.$emit('input', index)
+        }
+      }
+      this.$emit('click', event)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.button-group {
+  @include font-bold;
+  display: inline-block;
+  font-size: 14px;
+  user-select: none;
+
+  &__button {
+    border: 1px solid $black;
+    cursor: pointer;
+    display: inline-block;
+    padding-bottom: 0.25em;
+    padding-top: 0.35em;
+    text-align: center;
+    transition: all 0.2s ease-in-out;
+    width: 70px;
+  }
+
+  &__button:first-child {
+    border-bottom-left-radius: 4em;
+    border-top-left-radius: 4em;
+  }
+
+  &__button:last-child {
+    border-bottom-right-radius: 4em;
+    border-top-right-radius: 4em;
+  }
+
+  &__button:first-child::before,
+  &__button:last-child::after {
+    content: '';
+    display: inline-block;
+    width: 2px;
+  }
+
+  &__button:not(:last-child) {
+    border-right: 0;
+  }
+}
+
+@mixin colored($color, $value) {
+  .button-group--#{$color} {
+    .button-group__button {
+      border-color: $value;
+      color: $value;
+    }
+
+    .button-group__button[active] {
+      background-color: $value;
+      color: $white;
+    }
+  }
+}
+
+@include colored('white', $white);
+@include colored('black', $black);
+@include colored('gray', $gray);
+@include colored('gray-dark', $gray-dark);
+@include colored('gray-light', $gray-light);
+@include colored('gray-lighter', $gray-lighter);
+@include colored('gray-lighten', $gray-lighten);
+
+@include colored('red', $red);
+@include colored('red-dark', $red-dark);
+
+@include colored('pink', $pink);
+@include colored('cyan', $cyan);
+@include colored('aloes', $aloes);
+@include colored('orange', $orange);
+@include colored('mustard', $mustard);
+</style>
---
+++ app/router/paths/teacher/Teacher.vue
@@ -0,0 +1,42 @@
+<script>
+import ContentWrapper from '../../partial/ContentWrapper.vue'
+import DefaultNavbar from '../../partial/DefaultNavbar.vue'
+import DimiMenu from '../../../components/DimiMenu.vue'
+import DimiMenuItem from '../../../components/DimiMenuItem.vue'
+
+export default {
+  name: 'Teacher',
+
+  components: { ContentWrapper, DefaultNavbar, DimiMenu, DimiMenuItem },
+
+  created () {
+    // TODO check permission
+  }
+}
+</script>
+
+<template>
+  <div class="mng">
+    <default-navbar/>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-24 col-md-5">
+          <dimi-menu>
+            <dimi-menu-item :to="{ name: 'teacher' }">신청 현황</dimi-menu-item>
+            <dimi-menu-item :to="{ name: 'draft' }">동아리 가입 관리</dimi-menu-item>
+            <dimi-menu-item :to="{ name: 'teacher/afterschool' }">방과 후 활동 관리</dimi-menu-item>
+            <dimi-menu-item :to="{ name: 'draft' }">상담 관리</dimi-menu-item>
+            <dimi-menu-item :to="{ name: 'draft' }">인강실 사용 관리</dimi-menu-item>
+          </dimi-menu>
+        </div>
+        <main class="main col-xs">
+          <router-view/>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
