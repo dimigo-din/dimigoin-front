6b34c174658c19ab409e842814092c2e18cd7a89
Chalk <chalkpe@gmail.com>
2018-03-15 23:05:49 +0900

    add dotenv, move configs

--- app/app.js
+++ app/app.js
@@ -1,19 +1,21 @@
 import 'babel-polyfill'
-import Vue from 'vue'
+import dotenv from 'dotenv'
 
-import { sync } from 'vuex-router-sync'
-import Ripple from 'vue-ripple-directive'
+import Vue from 'vue'
 import swal from 'vue-sweetalert2'
+import Ripple from 'vue-ripple-directive'
+import { sync } from 'vuex-router-sync'
 
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
+dotenv.config()
 sync(store, router)
 
+Vue.use(swal)
 Ripple.zIndex = 55
 Vue.directive('ripple', Ripple)
-Vue.use(swal)
 
 console.debug(new Vue({
   store,
