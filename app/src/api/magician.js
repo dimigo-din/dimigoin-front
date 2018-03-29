a0add17ca11e9dd28814babebfd8b040830c636b
a1p4ca <dreamaker7770@gmail.com>
2018-03-29 09:21:57 +0900

    Implement default error handler of magician

--- app/src/api/magician.js
+++ app/src/api/magician.js
@@ -8,7 +8,8 @@ export default async (action, errorHandler) => {
     if (!err.res) throw new Error('네트워크에 문제가 있습니다.')
 
     const handler = errorHandler[err.status]
-    const defaultHandler = errorHandler['default'] || (() => {})
+    const defaultHandler = errorHandler['default'] ||
+      (() => new Error('알 수 없는 오류가 발생했으니 잠시 후 다시 시도해주세요.'))
 
     const value = handler ? handler() : defaultHandler()
 
