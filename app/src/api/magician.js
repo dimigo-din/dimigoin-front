9379148d114cd71440883f90bddb0d59ff4fd2f4
a1p4ca <dreamaker7770@gmail.com>
2018-04-05 01:02:53 +0900

    Improve magician

--- app/src/api/magician.js
+++ app/src/api/magician.js
@@ -7,11 +7,11 @@ export default async (action, errorHandler) => {
     console.warn(err)
     if (!err.res) throw new Error('네트워크에 문제가 있습니다.')
 
-    const handler = errorHandler[err.status]
-    const defaultHandler = errorHandler['default'] ||
+    const handler = errorHandler[err.status] ||
+      errorHandler['default'] ||
       (() => new Error('알 수 없는 오류가 발생했으니 잠시 후 다시 시도해주세요.'))
 
-    const value = handler ? handler() : defaultHandler()
+    const value = handler()
 
     if (value instanceof Error) throw value
     else return value
