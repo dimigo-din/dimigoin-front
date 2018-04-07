8acd91d13d717e68a5c0d1d5b2cde8e4cd3cefd1
a1p4ca <dreamaker7770@gmail.com>
2018-04-07 22:57:16 +0900

    Implement scalable structure

--- app/src/struct/assignment.js
+++ app/src/struct/assignment.js
@@ -7,40 +7,51 @@ const struct = superstruct({
   }
 })
 
+const _Author = struct.interface({
+  name: 'string',
+  studentSerial: 'string'
+})
+
+const _Report = struct.interface({
+  author: _Author,
+  uploadedDate: 'moment',
+  fileName: 'string'
+})
+
+const _Assignor = struct.interface({
+  name: 'string',
+  email: 'string'
+})
+
 const _Assignment = struct.interface({
   idx: 'number',
   title: 'string',
   description: 'string',
-  deadline: 'date',
+  deadline: 'moment',
   targetGrade: 'number',
   targetClass: 'number',
-  assignor: struct({
-    name: 'string',
-    email: 'string'
-  }),
-  reports: [struct.interface({
-    author: struct.interface({
-      name: 'string',
-      studentSerial: 'string'
-    }),
-    uploadedDate: 'moment',
-    fileName: 'string'
-  })]
+  assignor: _Assignor,
+  reports: struct.union([[_Report], 'undefined', 'null']),
+  report: struct.union([_Report, 'undefined', 'null'])
+})
+
+const convertReport = v => (v && {
+  author: {
+    name: v['author']['name'],
+    studentSerial: v['author']['student_serial']
+  },
+  uploadedDate: moment(v['uploaded_date']),
+  fileName: v['file_name']
 })
 
 export const Assignment = assignment => _Assignment({
   idx: assignment['idx'],
   title: assignment['title'],
+  deadline: moment(assignment['deadline']),
   description: assignment['deadline'],
   targetGrade: assignment['target_grade'],
   targetClass: assignment['target_class'],
   assignor: assignment['assignor'],
-  reports: assignment['reports'].map(v => ({
-    author: {
-      name: v['author']['name'],
-      studentSerial: v['author']['student_serial']
-    },
-    uploadedDate: moment(v['uploaded_date']),
-    fileName: v['file_name']
-  }))
+  reports: (assignment['reports'] || []).map(convertReport),
+  report: convertReport(assignment['report'])
 })
