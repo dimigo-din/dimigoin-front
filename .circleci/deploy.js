7e93f3fa829904bd26f52d1b3bb07d5aa549e7f3
a1p4ca <dreamaker7770@gmail.com>
2018-09-24 02:06:23 +0900

    Fix env

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -6,7 +6,7 @@ sftp({
   pass: process.env.SFTP_PASSWORD,
   host: process.env.SFTP_HOST,
   port: 22,
-  remotePath: process.env.SFTP_ROOT,
+  remotePath: process.env.SFTP_CWD,
   sourcePath: path.resolve(__dirname, '../dist')
 })
   .then(() => console.log('deploy finish'))
