6fc9252cd43c17f24fed05c6d429ee70fc2ee663
a1p4ca <dreamaker7770@gmail.com>
2018-10-18 22:58:55 +0900

    Fix api

--- app/src/api/ingang.js
+++ app/src/api/ingang.js
@@ -1,82 +1,100 @@
 import axios from './axios'
 import magician from './magician'
-import { StudentIngang, GradeIngang, Notice } from '@/src/struct/ingang'
+import { StudentIngang, GradeIngang } from '@/src/struct/ingang'
 
 function rightIngang (ingang) {
-  if (ingang[0].target_grade === 2) {
-    const klass = ingang[0].class
-    const today = new Date().getDay()
-    if (today % 2) {
-      if (klass % 2) {
-        return ingang.slice(0, 1)
-      } else {
-        return ingang.slice(1, 2)
-      }
-    } else {
-      if (klass % 2) {
-        return ingang.slice(1, 2)
-      } else {
-        return ingang.slice(0, 1)
-      }
-    }
-  } else {
-    return ingang
+  const grade = ingang[0].target_grade
+  const klass = ingang[0].class
+  const today = new Date().getDay()
+  return grade === 2 ? ((today + klass) % 2 ? ingang.slice(0, 1) : ingang.slice(1, 2)) : ingang
+}
+
+export const apply = {
+  async cancelIngang (idx) {
+    await magician(() => axios.delete(`/ingangs/request/${idx}`), {
+      401: () => new Error('존재하지 않는 학년 또는 반입니다.'),
+      403: () => new Error('신청 기간이 아닙니다.'),
+      404: () => new Error('신청이 조회되지 않았습니다.')
+    })
+  },
+
+  async applyIngang (idx) {
+    await magician(() => axios.post(`/ingangs/request/${idx}`), {
+      401: () => new Error('본인의 학년 또는 반이 아닙니다.'),
+      403: () => new Error('일주일 2회 신청을 모두 했습니다.'),
+      404: () => new Error('인강실 신청이 없습니다.'),
+      405: () => new Error('신청 기간이 아닙니다.'),
+      406: () => new Error('신청 가능한 시간이 아닙니다.'),
+      409: () => new Error('이미 신청을 했거나 인원이 꽉 찼습니다.')
+    })
+  },
+
+  async getStudentIngang () {
+    const ingang = await magician(() => axios.get('/ingangs/student/'), {
+      404: () => new Error('오늘은 인강실을 신청할 수 없습니다.')
+    })
+    const result = rightIngang(ingang)
+    return result.map(StudentIngang)
+  },
+
+  async getGradeIngang (grade) {
+    const { ingang } = await magician(() => axios.get(`/ingangs/${grade}`), {
+      403: () => new Error('권한이 없습니다.'),
+      404: () => new Error('인강 신청이 없습니다.')
+    })
+    return ingang.map(GradeIngang)
+  },
+
+  async getGradeTimeIngang (grade, time) {
+    const { ingang } = await magician(() => axios.get(`/ingangs/${grade}/${time}`), {
+      403: () => new Error('권한이 없습니다.'),
+      404: () => new Error('인강 신청이 없습니다.')
+    })
+    return ingang.map(GradeIngang)
   }
 }
 
-export async function applyIngang (idx) {
-  await magician(() => axios.post(`/ingangs/request/${idx}`), {
-    401: () => new Error('본인의 학년 또는 반이 아닙니다.'),
-    403: () => new Error('일주일 2회 신청을 모두 했습니다.'),
-    404: () => new Error('인강실 신청이 없습니다.'),
-    405: () => new Error('신청 기간이 아닙니다.'),
-    406: () => new Error('신청 가능한 시간이 아닙니다.'),
-    409: () => new Error('이미 신청을 했거나 인원이 꽉 찼습니다.')
-  })
+export const notice = {
+  async postNotice (notice) {
+    await magician(() => axios.post(`/ingang_notice`, notice), {})
+  },
+
+  async getNotices () {
+    var { notice } = await magician(() => axios.get(`/ingang_notice`), {})
+    notice.description.replace(/(?:\r\n|\r|\n)/g, '<br/>')
+    return notice
+  },
+
+  async getLatestNotice () {
+    var notice = await magician(() => axios.get(`/ingang_notice/latest`), {})
+    notice.description.replace(/(?:\r\n|\r|\n)/g, '<br/>')
+    return notice
+  }
 }
 
-export async function cancelIngang (idx) {
-  await magician(() => axios.delete(`/ingangs/request/${idx}`), {
-    401: () => new Error('존재하지 않는 학년 또는 반입니다.'),
-    403: () => new Error('신청 기간이 아닙니다.'),
-    404: () => new Error('신청이 조회되지 않았습니다.')
-  })
-}
+export const black = {
+  async getGradeBlack (grade) {
+    const { blacklist } = await magician(() => axios.get(`/ingang_black/grade/${grade}`), {})
+    return blacklist
+  },
 
-export async function getStudentIngang () {
-  const ingang = await magician(() => axios.get('/ingangs/student/'), {
-    404: () => new Error('오늘은 인강실을 신청할 수 없습니다.')
-  })
-  const result = rightIngang(ingang)
-  return result.map(StudentIngang)
-}
+  async getBlackLog (idx) {
+    const { date } = await magician(() => axios.get(`/ingang_black/log/${idx}`), {})
+    return date
+  },
 
-export async function getGradeIngang (grade) {
-  const { ingang } = await magician(() => axios.get(`/ingangs/${grade}`), {
-    403: () => new Error('권한이 없습니다.'),
-    404: () => new Error('인강 신청이 없습니다.')
-  })
-  return ingang.map(GradeIngang)
-}
+  async delBlack (idx) {
+    await magician(() => axios.delete(`/ingang_black/${idx}`), {
+      403: () => new Error('권한이 없습니다'),
+      404: () => new Error('블랙리스트가 아닙니다.')
+    })
+  },
 
-export async function getGradeTimeIngang (grade, time) {
-  const { ingang } = await magician(() => axios.get(`/ingangs/${grade}/${time}`), {
-    403: () => new Error('권한이 없습니다.'),
-    404: () => new Error('인강 신청이 없습니다.')
-  })
-  return ingang.map(GradeIngang)
-}
+  async postBlack (idx) {
+    await magician(() => axios.post(`/ingang_black/${idx}`), {})
+  },
 
-export async function addNotice (notice) {
-  await magician(() => axios.post(`/ingangs/notice`, notice), {})
-}
-
-export async function getNotices () {
-  const { notice } = await magician(() => axios.get(`/ingangs/notice`), {})
-  return notice.map(Notice)
-}
-
-export async function getLatestNotice () {
-  const notice = await magician(() => axios.get(`/ingangs/notice_latest`), {})
-  return Notice(notice)
+  async getStudentBlack (idx) {
+    await magician(() => axios.get(`/ingang_black/${idx}`), {})
+  }
 }
