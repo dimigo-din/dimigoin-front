85657b53f3373399fbdfc10588e5afdeb0f46889
a1p4ca <dreamaker7770@gmail.com>
2019-01-14 02:13:57 +0900

    Give Sentry more information about users

--- app/store/modules/account/informations.js
+++ app/store/modules/account/informations.js
@@ -1,4 +1,5 @@
 import { UPDATE_INFO, RESET_INFO } from './mutation-types'
+import * as Sentry from '@sentry/browser'
 
 export default {
   state: {
@@ -21,6 +22,17 @@ export default {
   mutations: {
     [UPDATE_INFO] (state, payload) {
       Object.keys(payload).forEach(v => (state[v] = payload[v]))
+
+      Sentry.configureScope(scope => {
+        scope.setUser({
+          id: payload.id,
+          userIdx: payload.idx,
+          username: payload.name,
+          serial: payload.serial,
+          photoUrl: payload.photoUrl,
+          userType: payload.userType
+        })
+      })
     },
 
     [RESET_INFO] (state) {
@@ -36,6 +48,8 @@ export default {
         klass: 0,
         number: 0
       })
+
+      Sentry.configureScope(scope => scope.setUser({}))
     }
   }
 }
