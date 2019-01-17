0b6f51e5d3113afa2d07f793aae8be874db5ef41
a1p4ca <dreamaker7770@gmail.com>
2019-01-17 16:15:48 +0900

    Update vue-loader options

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,7 +49,9 @@ const webpackConfig = {
         use: {
           loader: 'vue-loader',
           options: {
-            extractCSS: true
+            compilerOptions: {
+              whitespace: 'condense' // This option will affect after vue version 2.6 >=
+            }
           }
         }
       },
