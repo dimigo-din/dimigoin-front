b316998fa5fdb77cddcfdaaa076ad6d1048748cf
a1p4ca <dreamaker7770@gmail.com>
2019-01-21 02:51:07 +0900

    Replace grid in Menu Wrappers with ServiceWrapper

---
+++ app/components/ServiceWrapper.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  name: 'ServiceWrapper'
+}
+</script>
+
+<template>
+  <div>
+    <slot name="navbar" />
+    <div class="container">
+      <div class="wrapper">
+        <div class="wrapper__menu">
+          <slot name="menu" />
+        </div>
+        <main class="wrapper__main">
+          <router-view />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  box-sizing: border-box;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-left: -1rem;
+  margin-right: -1rem;
+
+  &__menu {
+    box-sizing: border-box;
+    flex: 0 0 auto;
+    flex-basis: 100%;
+    max-width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    @include from($desktop) {
+      flex-basis: 21%;
+      max-width: 21%;
+    }
+  }
+
+  &__main {
+    box-sizing: border-box;
+    flex: 0 0 auto;
+    flex-basis: 0;
+    flex-grow: 1;
+    max-width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+}
+</style>
--- app/pages/paths/management/Manage.vue
+++ app/pages/paths/management/Manage.vue
@@ -1,41 +1,32 @@
 <script>
 import DefaultNavbar from '@/components/DefaultNavbar.vue'
+import ServiceWrapper from '@/components/ServiceWrapper.vue'
 
 export default {
   name: 'Wrapper',
-
-  components: { DefaultNavbar }
+  components: { DefaultNavbar, ServiceWrapper }
 }
 </script>
 
 <template>
-  <div>
-    <default-navbar />
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-24 col-md-5">
-          <dimi-menu>
-            <dimi-menu-item :to="{ name: 'management' }">
-              관리 페이지
-            </dimi-menu-item>
-            <dimi-menu-item :to="{ name: 'management/circle' }">
-              동아리 신청 관리
-            </dimi-menu-item>
-            <dimi-menu-item :to="{ name: 'management/dets' }">
-              Dets 관리
-            </dimi-menu-item>
-            <dimi-menu-item :to="{ name: 'management/book' }">
-              도서 신청 관리
-            </dimi-menu-item>
-            <dimi-menu-item :to="{ name: 'management/freshman' }">
-              신입생 공지 관리
-            </dimi-menu-item>
-          </dimi-menu>
-        </div>
-        <main class="main col-xs">
-          <router-view />
-        </main>
-      </div>
-    </div>
-  </div>
+  <service-wrapper>
+    <default-navbar slot="navbar" />
+    <dimi-menu slot="menu">
+      <dimi-menu-item :to="{ name: 'management' }">
+        관리 페이지
+      </dimi-menu-item>
+      <dimi-menu-item :to="{ name: 'management/circle' }">
+        동아리 신청 관리
+      </dimi-menu-item>
+      <dimi-menu-item :to="{ name: 'management/dets' }">
+        Dets 관리
+      </dimi-menu-item>
+      <dimi-menu-item :to="{ name: 'management/book' }">
+        도서 신청 관리
+      </dimi-menu-item>
+      <dimi-menu-item :to="{ name: 'management/freshman' }">
+        신입생 공지 관리
+      </dimi-menu-item>
+    </dimi-menu>
+  </service-wrapper>
 </template>
