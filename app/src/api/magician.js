4579cf21480cd905604df6a6c243cb91696f05cb
a1p4ca <dreamaker7770@gmail.com>
2019-02-09 18:36:56 +0900

    Improve magician

--- app/src/api/magician.js
+++ app/src/api/magician.js
@@ -1,18 +1,34 @@
-export default async (action, errorHandler = {}) => {
+import APIError from '@/src/errors/api-error'
+import NetworkError from '@/src/errors/network-error'
+
+/**
+ * Handles axios error
+ *
+ * @typedef {AxiosResponse & { error: (AxiosError | undefined) }} CustomResponse
+ * @function magician
+ * @param {function(): Promise<CustomResponse, AxiosError>} action
+ * @param {Object<number, (string | function(AxiosError): *)>} [errorHandler={}]
+ * @returns {CustomResponse}
+ * @throws {Error}
+ */
+export default async function magician (action, errorHandler = {}) {
   try {
-    const res = await action()
-
-    return res
+    return await action()
   } catch (err) {
-    if (!err.response) return Promise.reject(err)
+    if (!err.response) throw new NetworkError()
 
-    const handler = errorHandler[err.response.status] ||
-      errorHandler['default'] ||
-      (() => err)
+    const handler = errorHandler[err.response.status] || errorHandler['default']
+
+    if (!handler) throw err
+    if (typeof handler === 'string') throw new APIError(handler, err.response.status)
+    if (typeof handler !== 'function') throw new TypeError('type of errorHandler is invalid')
 
     const value = handler(err)
 
-    if (value instanceof Error) return Promise.reject(value)
-    else return { data: value }
+    return {
+      ...err.response,
+      error: err,
+      data: value instanceof Promise ? await value : value
+    }
   }
 }
