707a804f93049dec476e63064d3d8fa2c86e0293
taehoon02 <taevel02@gmail.com>
2019-02-17 15:57:06 +0900

    Create dimiru component - DimiError

---
+++ app/dimiru/components/DimiError.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'DimiError',
+
+  props: {
+    errorMessage: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="error-wrapper">
+    <p
+      v-if="errorMessage"
+      class="error"
+    >
+      <slot />
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../scss/vars';
+
+.error-wrapper {
+  position: relative;
+  width: 100%;
+
+  .error {
+    position: relative;
+    width: 100%;
+    padding-left: 1em;
+    margin-top: 0.5em;
+    color: $red;
+    font-size: 12px;
+  }
+}
+
+</style>
