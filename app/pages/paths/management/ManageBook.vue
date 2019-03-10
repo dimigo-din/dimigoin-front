93b2e44645d71e3af0d05acd4f907284ccc1d33b
Uhm Seohun <uhmtotow@gmail.com>
2019-03-11 07:52:23 +0900

    fix(style): remove useless style of last item in book management (#125)
    * fix(style): remove useless padding
    
    * fix(style): remove useless horizontal line
    
    * fix(style): remove duplicated border line

--- app/pages/paths/management/ManageBook.vue
+++ app/pages/paths/management/ManageBook.vue
@@ -119,7 +119,10 @@ export default {
           :key="`${idx}`"
         >
           <div
-            class="book__book"
+            :class="{
+              'book__book': true,
+              'book__border': idx
+            }"
             @click="book.open = !book.open"
           >
             <dimi-badge
@@ -244,6 +247,7 @@ export default {
 
   &__main {
     padding-top: 0;
+    padding-bottom: 0;
   }
 
   &__book {
@@ -251,8 +255,11 @@ export default {
     align-items: center;
     justify-content: flex-start;
     padding: 24px;
-    border-bottom: 1px solid $gray-lighter;
     cursor: pointer;
+  }
+
+  &__border {
+    border-top: 1px solid $gray-lighter;
   }
 
   &__item {
@@ -276,7 +283,6 @@ export default {
     align-items: stretch;
     justify-content: flex-start;
     padding: 24px;
-    border-bottom: 1px solid $gray-lighter;
     cursor: default;
   }
 
