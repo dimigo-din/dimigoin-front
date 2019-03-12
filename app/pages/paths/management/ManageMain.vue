f77bfbc4ec28568b3b725d1532054dd1da76734e
alpaca <dreamaker7770@gmail.com>
2019-03-13 08:09:36 +0900

    fix(browser): support old browser (#149)
    * fix(browser): support old browser
    
    * feat: merge and remap teacher and management

--- app/pages/paths/management/ManageMain.vue
+++ app/pages/paths/management/ManageMain.vue
@@ -1,19 +1,13 @@
 <script>
-import permission from '@/mixins/permission'
-
-import services from './services'
-
 export default {
-  name: 'TeacherMain',
-  mixins: [permission('T')],
-  data: () => ({ services }),
-  mounted () {
-    const cards = this.$refs.cards || []
-    cards.forEach(({ $el: v }) => (v.style.height = window.getComputedStyle(v).width))
-  },
+  name: 'ManageMain',
+  data: () => ({
+    managements: []
+  }),
+
   methods: {
     clickService (service) {
-      this.$router.push({ name: service.to })
+
     }
   }
 }
