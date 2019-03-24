3a73d6f712d33c4f4e0a5ce1e2a49acdc82216ef
Taehoon (Taehoon Kwon) <taevel02@gmail.com>
2019-03-25 00:57:11 +0900

    refactor: reform ingang service (#179)
    * refactor: ingang service in request
    
    * refactor: change ingang api
    
    * refactor: change ingang request, management page
    
    * refactor: add and delete ingang tab in management

--- app/pages/paths/management/ManageIngang.vue
+++ app/pages/paths/management/ManageIngang.vue
@@ -1,6 +1,8 @@
 <script>
 import ContentWrapper from '@/components/ContentWrapper.vue'
 
+import days from '@/src/util/days'
+import timestamp from 'unix-timestamp'
 import { ingangManager } from '@/src/api/ingang'
 
 export default {
@@ -9,11 +11,60 @@ export default {
   data: () => ({
     pending: false,
     currentTab: 0,
-    notice: '',
+    checks: [],
+    ingangs: [],
+    notice: {
+      grade: '',
+      desc: ''
+    },
     modal: {
       profile: {}
+    },
+
+    form: {
+      grade: '',
+      day: 0,
+      time: '',
+      date: new Date(),
+      startDate: new Date(),
+      endDate: new Date(),
+      max_user_1: '',
+      max_user_2: '',
+      max_user_3: '',
+      max_user_4: '',
+      max_user_5: '',
+      max_user_6: ''
     }
   }),
+
+  computed: {
+    days () {
+      return days
+    },
+
+    ingangInput () {
+      return {
+        grade: this.form.grade,
+        day: days[this.form.day].code,
+        time: this.form.time,
+        startDate: timestamp.fromDate(this.form.startDate),
+        endDate: timestamp.fromDate(this.form.endDate),
+        date: timestamp.fromDate(this.form.date),
+        max_user_1: parseInt(this.form.max_user_1),
+        max_user_2: parseInt(this.form.max_user_2),
+        max_user_3: parseInt(this.form.max_user_3),
+        max_user_4: parseInt(this.form.max_user_4),
+        max_user_5: parseInt(this.form.max_user_5),
+        max_user_6: parseInt(this.form.max_user_6)
+      }
+    }
+  },
+
+  watch: {
+    selectAll (val) {
+      this.checks = this.checks.map(() => val)
+    }
+  },
 
   async created () {
     this.refresh()
@@ -22,14 +73,15 @@ export default {
   methods: {
     async refresh () {
       this.pending = true
-      this.notice = (await ingangManager.getAnnouncement()).description
+      this.ingangs = await ingangManager.getIngangs()
+      this.notice.desc = (await ingangManager.getAnnouncement()).description
       this.pending = false
     },
 
     restructure (notice) {
       return {
-        'description': notice,
-        'date': this.timezone(new Date()).toISOString()
+        'description': notice.desc,
+        'grade': notice.grade
       }
     },
 
@@ -43,12 +95,30 @@ export default {
       }
     },
 
-    async download (grade) {
+    async addIngang () {
+      try {
+        await ingangManager.createIngang(this.ingangInput)
+        this.$swal('성공!', '추가되었습니다.', 'success')
+
+        this.form.repeat = false
+
+        await this.refresh()
+      } catch (err) {
+        this.$swal('이런!', err.message, 'error')
+      }
+    },
+
+    async downloadExcel (grade) {
       try {
         await ingangManager.downloadExcel(grade)
       } catch (err) {
         this.$swal('이런!', err.message, 'error')
       }
+    },
+
+    async deleteIngang (idx) {
+      await ingangManager.deleteIngang(idx)
+      await this.refresh()
     },
 
     timezone (val) {
@@ -70,7 +140,7 @@ export default {
     >
       <dimi-tab
         v-model="currentTab"
-        :tabs="['엑셀', '공지']"
+        :tabs="['엑셀', '공지', '인강실']"
       />
       <div
         v-if="pending"
@@ -86,14 +156,14 @@ export default {
           <dimi-button
             :loading="pending"
             class="excel__item"
-            @click="download(1)"
+            @click="downloadExcel(1)"
           >
             1학년 엑셀 다운
           </dimi-button>
           <dimi-button
             :loading="pending"
             class="excel__item"
-            @click="download(2)"
+            @click="downloadExcel(2)"
           >
             2학년 엑셀 다운
           </dimi-button>
@@ -102,8 +172,15 @@ export default {
           v-if="currentTab === 1"
         >
           <div class="notice">
+            <dimi-input
+              v-model="notice.grade"
+              class="notice__input"
+              placeholder="공지사항을 보내고자 하는 학년을 입력해주세요."
+            />
+          </div>
+          <div class="notice">
             <dimi-long-input
-              v-model="notice"
+              v-model="notice.desc"
               :height="300"
               class="notice__input"
             />
@@ -117,12 +194,305 @@ export default {
             </dimi-button>
           </div>
         </div>
+        <div
+          v-if="currentTab === 2"
+        >
+          <section class="mng-ing__section">
+            <h2 class="mng-ing__title">
+              인강실 추가/삭제
+            </h2>
+
+            <table class="mng-ing__list">
+              <tbody>
+                <tr
+                  v-for="(item, index) in ingangs"
+                  :key="index"
+                  class="mng-ing__row"
+                >
+                  <td class="mng-ing__cell">
+                    {{ item.day }}
+                  </td>
+                  <td class="mng-ing__cell">
+                    {{ item.grade }}학년 {{ item.klass }}반
+                  </td>
+                  <td class="mng-ing__cell mng-ing__cell--time">
+                    {{ item.time }}타임
+                  </td>
+                  <td class="mng-ing__cell">
+                    총 {{ item.max }}명
+                  </td>
+                  <td class="mng-ing__cell">
+                    {{ item.count }}명 신청
+                  </td>
+                  <td class="mng-ing__cell">
+                    <span
+                      class="mng-ing__tool mng-ing__delete"
+                      @click="deleteIngang(item.idx)"
+                    >
+                      <span class="mng-ing__delete-icon icon-delete" /> 삭제
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+
+          <section class="mng-ing__section">
+            <h2 class="mng-ing__title">
+              인강실 추가
+            </h2>
+            <div class="mng-ing__form">
+              <div class="mng-ing__form-row">
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    학년
+                  </label>
+                  <dimi-input
+                    v-model="form.grade"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    타임
+                  </label>
+                  <dimi-input
+                    v-model="form.time"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    날짜
+                  </label>
+                  <dimi-input
+                    v-model="form.date"
+                    type="date"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    요일
+                  </label>
+                  <dimi-dropdown
+                    v-model="form.day"
+                    :items="days.map(v => v.text)"
+                    :dropup="true"
+                    class="mng-ing__input mng-ing__input--day"
+                  />
+                </div>
+              </div>
+
+              <div class="mng-ing__form-row">
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    1반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_1"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    2반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_2"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    3반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_3"
+                    class="mng-ing__input"
+                  />
+                </div>
+              </div>
+
+              <div class="mng-ing__form-row">
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    4반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_4"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    5반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_5"
+                    class="mng-ing__input"
+                  />
+                </div>
+
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    6반 최대인원
+                  </label>
+                  <dimi-input
+                    v-model="form.max_user_6"
+                    class="mng-ing__input"
+                  />
+                </div>
+              </div>
+
+              <div class="mng-ing__form-row">
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    신청 시작
+                  </label>
+                  <dimi-date-input v-model="form.startDate" />
+                </div>
+              </div>
+
+              <div class="mng-ing__form-row">
+                <div class="mng-ing__field">
+                  <label class="mng-ing__label">
+                    신청 마감
+                  </label>
+                  <dimi-date-input v-model="form.endDate" />
+                </div>
+              </div>
+
+              <div class="mng-ing__form-row mng-ing__form-row--submit">
+                <div class="mng-ing__field mng-ing__field--right">
+                  <dimi-button @click="addIngang">
+                    추가하기
+                  </dimi-button>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </template>
     </dimi-card>
   </content-wrapper>
 </template>
 
 <style lang="scss" scoped>
+.mng-ing {
+  &__section {
+    padding: 0 24px 24px;
+    margin-top: 36px;
+    margin-bottom: 24px;
+  }
+
+  &__section:last-child {
+    padding-bottom: 0;
+  }
+
+  &__title {
+    margin-bottom: 48px;
+    color: $gray-dark;
+    font-size: 24px;
+    font-weight: $font-weight-bold;
+  }
+
+  &__delete {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+  }
+
+  &__delete-icon {
+    font-size: 18px;
+  }
+
+  &__list {
+    display: block;
+    width: 100%;
+    height: 600px;
+    color: $gray !important;
+    font-weight: $font-weight-bold;
+    overflow-y: auto;
+  }
+
+  &__row:not(:last-child) {
+    border-bottom: 1px solid $gray-lighter;
+  }
+
+  &__cell {
+    padding: 24px;
+    white-space: nowrap;
+  }
+
+  &__cell--time {
+    width: 99%;
+    line-height: 1.5;
+    white-space: normal;
+  }
+
+  &__form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  &__form-row {
+    display: flex;
+    margin-bottom: 24px;
+  }
+
+  &__form-row--submit {
+    margin-top: 10px;
+    margin-bottom: 24px;
+  }
+
+  &__field {
+    display: flex;
+    align-items: center;
+  }
+
+  &__field--full {
+    flex-grow: 1;
+  }
+
+  &__field--right {
+    margin-left: auto;
+  }
+
+  &__label {
+    width: 5em;
+    max-width: 4em;
+    padding-right: 1em;
+    text-align: right;
+  }
+
+  &__input {
+    font-size: 16px;
+  }
+
+  &__input--manager {
+    width: 10em;
+  }
+
+  &__input--maxCount {
+    width: 6em;
+  }
+
+  &__input--day {
+    padding: 0.75em 1em;
+    border: 0;
+    background-color: $gray-lighten;
+    border-radius: 20px;
+  }
+}
+
 .ingang {
   padding: 0;
   border: 0;
