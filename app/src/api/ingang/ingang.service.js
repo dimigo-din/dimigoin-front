62dbbcd4ea32e4b645022b1346f80fb368b6abc7
Taehoon (Taehoon Kwon) <taevel02@gmail.com>
2019-03-24 20:46:31 +0900

    fix: check permission downloading excel file in management (#169)
    * fix: check permission download excel file in management
    
    * fix: edit function name more clearly
    
    * chore: rename file and remove a tag after excel download
    
    * refactor: use date-fns module in formatting file name
    
    * refactor: rename excel download function name.

--- app/src/api/ingang/ingang.service.js
+++ app/src/api/ingang/ingang.service.js
@@ -1,4 +1,5 @@
 import { ServiceBase } from '@/src/api/service-base'
+import { format } from 'date-fns'
 
 class IngangService extends ServiceBase {
   /**
@@ -106,5 +107,20 @@ export class IngangManagerService extends IngangService {
    */
   async editAnnouncement (idx, notice) {
     // TODO
+  }
+
+  async downloadExcel (grade) {
+    const { data } = await this.magician(() => this.r.get(`/excel/${grade}`, {
+      responseType: 'blob'
+    }), {
+      403: '권한이 없습니다.',
+      default: '파일을 다운로드하던 중 문제가 발생했습니다.'
+    })
+    const link = document.createElement('a')
+    link.href = window.URL.createObjectURL(new Blob([data]))
+    link.setAttribute('download', `${grade}학년 ${format(new Date(), 'YYYY')}년 ${format(new Date(), 'MM')}월 ${format(new Date(), 'DD')}일 인강실.xlsx`)
+    document.body.appendChild(link)
+    link.click()
+    link.remove()
   }
 }
