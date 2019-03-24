6afc1ef9f1c9d00969de9bcf621a5f92f7871806
taehoon02 <taevel02@gmail.com>
2019-03-24 21:26:42 +0900

    refactor: change ingang api

--- app/src/api/ingang/ingang.service.js
+++ app/src/api/ingang/ingang.service.js
@@ -1,9 +1,9 @@
 import ValidationError from '@/src/errors/validation-error'
 import { ServiceBase } from '@/src/api/service-base'
-import { Ingang, CreateIngangInput, Announcement } from './ingang.struct'
+import { Ingang, Status, CreateIngangInput, Announcement } from './ingang.struct'
 
 function tempValidation (ingang) {
-  const keys = ['target_grade', 'day', 'time', 'request_start_date', 'request_end_date', 'date', '1_max_user', '2_max_user', '3_max_user', '4_max_user', '5_max_user', '6_max_user']
+  const keys = ['target_grade', 'time', 'date', 'day', '1_max_user', '2_max_user', '3_max_user', '4_max_user', '5_max_user', '6_max_user', 'request_start_date', 'request_end_date']
   const missingArguments = keys.filter(key => !ingang[key])
   if (missingArguments.length > 0) {
     throw new ValidationError('모든 입력란을 채워주세요.', missingArguments)
@@ -34,13 +34,22 @@ export class IngangRequestorService extends IngangService {
   }
 
   /**
+   * 자신의 신청 현황을 반환합니다.
+   *
+   * @returns {Promise<{daily_request_count, weekly_ticket_num, daily_ticket_num, weekly_request_count}>}
+   */
+  async getStatus () {
+    const { data: status } = await this.magician(() => this.r.get(`/status`), {})
+    return Status(status)
+  }
+
+  /**
    * 자신이 해당 인강실을 신청합니다.
    *
    * @param idx
    */
   async applyIngang (idx) {
-    await this.magician(() => this.r.post(`/`, idx), {
-      401: '알맞지 않은 인강실 신청입니다.',
+    await this.magician(() => this.r.post(`/${idx}`), {
       403: '모든 티켓을 사용했습니다.',
       404: '존재하지 않는 인강실 신청입니다.',
       405: '신청 기간이 아닙니다.',
@@ -54,15 +63,22 @@ export class IngangRequestorService extends IngangService {
    * @param idx
    */
   async cancelIngang (idx) {
-    await this.magician(() => this.r.delete(`/`, idx), {
-      401: '알맞지 않은 인강실 신청입니다.',
-      403: '신청 기간이 아니거나 모든 티켓을 사용하였습니다.',
+    await this.magician(() => this.r.delete(`/${idx}`), {
+      403: '모든 티켓을 사용했습니다.',
       404: '존재하지 않는 인강실 신청입니다.'
     })
   }
 }
 
 export class IngangManagerService extends IngangService {
+  /**
+   * 모든 인강실을 반환합니다.
+   */
+  async getIngangs () {
+    const { data: { ingangs } } = await this.magician(() => this.r.get(`/admin`), {})
+    return ingangs.map(Ingang)
+  }
+
   /**
    * 인강실을 추가합니다.
    *
@@ -83,7 +99,7 @@ export class IngangManagerService extends IngangService {
    * @param idx
    */
   async deleteIngang (idx) {
-    await this.magician(() => this.r.post(`/admin`, idx), {
+    await this.magician(() => this.r.post(`/admin/${idx}`), {
       400: '잘못된 입력입니다.',
       403: '권한이 없습니다.'
     })
--- app/src/api/ingang/ingang.struct.js
+++ app/src/api/ingang/ingang.struct.js
@@ -1,16 +1,19 @@
 import timestamp from 'unix-timestamp'
 
 export const Ingang = ingang => ({
-  ingangs: {
-    idx: ingang['idx'],
-    day: ingang['day'],
-    time: ingang['time'],
-    startDate: timestamp.toDate(ingang['request_start_date']),
-    endDate: timestamp.toDate(ingang['request_end_date']),
-    status: ingang['status'],
-    count: ingang['present'],
-    max: ingang['max_user']
-  },
+  idx: ingang['idx'],
+  grade: ingang['target_grade'],
+  klass: ingang['klass'],
+  day: ingang['day'],
+  time: ingang['time'],
+  startDate: timestamp.toDate(ingang['request_start_date']),
+  endDate: timestamp.toDate(ingang['request_end_date']),
+  status: ingang['status'],
+  count: ingang['present'],
+  max: ingang['max_user']
+})
+
+export const Status = ingang => ({
   weekly_request_count: ingang['weekly_request_count'],
   daily_request_count: ingang['daily_request_count'],
   weekly_ticket_num: ingang['weekly_ticket_num'],
@@ -23,7 +26,13 @@ export const CreateIngangInput = ingang => ({
   'time': ingang.time,
   'request_start_date': ingang.startDate,
   'request_end_date': ingang.endDate,
-  'date': ingang.date
+  'date': timestamp.toDate(ingang['date']),
+  '1_max_user': ingang['1_max_user'],
+  '2_max_user': ingang['2_max_user'],
+  '3_max_user': ingang['3_max_user'],
+  '4_max_user': ingang['4_max_user'],
+  '5_max_user': ingang['5_max_user'],
+  '6_max_user': ingang['6_max_user']
 })
 
 export const Announcement = notice => ({
