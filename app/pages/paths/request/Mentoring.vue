81de2c66013fb3418c9551c34dbf7ba654eeb389
uhmtoto <uhmtotow@gmail.com>
2019-04-08 09:40:58 +0900

    fix: mentoring startTime, endTime, date information

--- app/pages/paths/request/Mentoring.vue
+++ app/pages/paths/request/Mentoring.vue
@@ -117,7 +117,7 @@ export default {
                 {{ item.subject }}
               </td>
               <td class="mentoring__cell">
-                {{ item.startTime | filterDay }} ({{ getDaySmallText(item.day) }})
+                {{ item.date | filterDay }} ({{ getDaySmallText(item.day) }})
               </td>
               <td
                 :class="{
@@ -126,7 +126,7 @@ export default {
                 }"
                 class="mentoring__cell"
               >
-                {{ item.startTime | filterTime }} ~ {{ item.endTime | filterTime }}
+                {{ item.startTime }} ~ {{ item.endTime }}
               </td>
               <td
                 :class="{
