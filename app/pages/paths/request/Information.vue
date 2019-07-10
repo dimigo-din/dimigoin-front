f3dfd8a565e6a9346d356163ac797999e51c134b
Seohun Uhm <uhmtotow@gmail.com>
2019-07-10 16:15:47 +0900

    feat: change priority of requests information (#306)
    동아리 신청 맨 뒤로 뺌 (requestor도)

--- app/pages/paths/request/Information.vue
+++ app/pages/paths/request/Information.vue
@@ -59,9 +59,9 @@ export default {
   },
 
   async created () {
-    this.circles = await circleRequestor.getCircles()
     this.ingangs = (await ingangRequestor.getIngangs()).filter(v => v.status)
     this.afters = (await afterschool.getStudentAfterschool()).filter(v => v.status)
+    this.circles = await circleRequestor.getCircles()
   }
 }
 </script>
@@ -71,64 +71,6 @@ export default {
     <h1 slot="header">
       <span class="icon-list" />나의 신청 현황
     </h1>
-
-    <dimi-card
-      slot="main"
-      class="r-info"
-    >
-      <dimi-badge
-        :color="isCircleApplied ? 'aloes' : 'orange'"
-        class="r-info__badge"
-      >
-        <template v-if="isCircleApplied">
-          <span class="icon-ok r-info__badge-icon" /> 신청
-        </template>
-        <template v-else>
-          <span class="icon-cross r-info__badge-icon" /> 미신청
-        </template>
-      </dimi-badge>
-
-      <span class="r-info__title">
-        <span class="icon-club-sm" /> 2019년 동아리 가입 신청
-      </span>
-
-      <table class="r-info__list">
-        <tbody>
-          <tr v-if="circleGroup.wait.length > 0">
-            <td class="r-info__list-key">
-              대기 중
-            </td>
-            <td class="r-info__list-value">
-              {{ circleGroup.wait.join(', ') }}
-            </td>
-          </tr>
-          <tr v-if="circleGroup.accept.length > 0">
-            <td class="r-info__list-key">
-              합격
-            </td>
-            <td class="r-info__list-value">
-              {{ circleGroup.accept.join(', ') }}
-            </td>
-          </tr>
-          <tr v-if="circleGroup.fail.length > 0">
-            <td class="r-info__list-key">
-              불합격
-            </td>
-            <td class="r-info__list-value">
-              {{ circleGroup.fail.join(', ') }}
-            </td>
-          </tr>
-          <tr v-if="circleGroup.final.length > 0">
-            <td class="r-info__list-key">
-              최종 결정
-            </td>
-            <td class="r-info__list-value">
-              {{ circleGroup.final.join(', ') }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </dimi-card>
 
     <dimi-card
       slot="main"
@@ -187,6 +129,64 @@ export default {
               class="r-info__list-value"
             >
               오늘은 방과후 수업이 없습니다.
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </dimi-card>
+
+    <dimi-card
+      slot="main"
+      class="r-info"
+    >
+      <dimi-badge
+        :color="isCircleApplied ? 'aloes' : 'orange'"
+        class="r-info__badge"
+      >
+        <template v-if="isCircleApplied">
+          <span class="icon-ok r-info__badge-icon" /> 신청
+        </template>
+        <template v-else>
+          <span class="icon-cross r-info__badge-icon" /> 미신청
+        </template>
+      </dimi-badge>
+
+      <span class="r-info__title">
+        <span class="icon-club-sm" /> 2019년 동아리 가입 신청
+      </span>
+
+      <table class="r-info__list">
+        <tbody>
+          <tr v-if="circleGroup.wait.length > 0">
+            <td class="r-info__list-key">
+              대기 중
+            </td>
+            <td class="r-info__list-value">
+              {{ circleGroup.wait.join(', ') }}
+            </td>
+          </tr>
+          <tr v-if="circleGroup.accept.length > 0">
+            <td class="r-info__list-key">
+              합격
+            </td>
+            <td class="r-info__list-value">
+              {{ circleGroup.accept.join(', ') }}
+            </td>
+          </tr>
+          <tr v-if="circleGroup.fail.length > 0">
+            <td class="r-info__list-key">
+              불합격
+            </td>
+            <td class="r-info__list-value">
+              {{ circleGroup.fail.join(', ') }}
+            </td>
+          </tr>
+          <tr v-if="circleGroup.final.length > 0">
+            <td class="r-info__list-key">
+              최종 결정
+            </td>
+            <td class="r-info__list-value">
+              {{ circleGroup.final.join(', ') }}
             </td>
           </tr>
         </tbody>
