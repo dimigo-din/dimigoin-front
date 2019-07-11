093ea4c6d1393ecfe036c634aa5c62e141e4bcac
uhmtoto <uhmtotow@gmail.com>
2019-07-11 14:22:41 +0900

    feat: change assignment submit page title

--- app/pages/paths/assignment/Assignment.vue
+++ app/pages/paths/assignment/Assignment.vue
@@ -75,7 +75,7 @@ export default {
 
       <span slot="header">
         <span class="assignee__header">
-          <span class="icon-submission" />과제 제출 관리
+          <span class="icon-submission" />과제 제출
 
           <dimi-checkbox
             v-model="filter.deadline"
