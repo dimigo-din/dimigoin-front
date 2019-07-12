079a2f7305a15784ea249c656486a7db6fed58e0
Seohun Uhm <uhmtotow@gmail.com>
2019-07-12 21:25:44 +0900

    Refactor assignment (#313)
    * refactor: change assignment struct name
    
    * refactor: use camel case in assignment structure
    
    * feat: add create and edit assignment input structure
    
    * refactor: return structured assignments in service
    
    * refactor: use camel case in assignment base
    
    * refactor: remove timestamp module in assignment base component
    
    * fix: assignment deadline filtering logic
    
    * feat: change assignment submit page title
    
    * refactor: assignment management logic and structure
    
    * fix: implement number modifier to type cast
    
    * fix: use date instead of unix timestamp

--- app/src/api/assignment/assignment.struct.js
+++ app/src/api/assignment/assignment.struct.js
@@ -10,13 +10,29 @@ const convertReport = v => (v && {
   fileName: v['file_name']
 })
 
-export const Assignments = assignment => ({
+export const Assignment = assignment => ({
   idx: assignment['idx'],
   title: assignment['title'],
   description: assignment['description'],
-  end_date: timestamp.toDate(assignment['end_date']),
+  endDate: timestamp.toDate(assignment['end_date']),
   targetGrade: assignment['target_grade'],
   targetClass: assignment['target_class'],
   assignor: assignment['assignor'],
   reports: (assignment['reports'] || []).map(convertReport)
+})
+
+export const CreateAssignmentInput = assignment => ({
+  'title': assignment.title,
+  'description': assignment.description,
+  'end_date': timestamp.fromDate(assignment.endDate),
+  'target_grade': assignment.targetGrade,
+  'target_class': assignment.targetClass
+})
+
+export const EditAssignmentInput = assignment => ({
+  'title': assignment.title,
+  'description': assignment.description,
+  'end_date': timestamp.fromDate(assignment.endDate),
+  'target_grade': assignment.targetGrade,
+  'target_class': assignment.targetClass
 })
