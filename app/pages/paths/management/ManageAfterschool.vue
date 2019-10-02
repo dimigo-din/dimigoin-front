ef0ce588f34822585976a5d5720a7a07f5aefdd8
Seohun Uhm <uhmtotow@gmail.com>
2019-10-02 10:53:32 +0900

    feat: show old afterschool request term (#364)
    * feat: show old request term
    
    * style: change priority of filter in MngAfsc

--- app/pages/paths/management/ManageAfterschool.vue
+++ app/pages/paths/management/ManageAfterschool.vue
@@ -2,11 +2,16 @@
 import ContentWrapper from '@/components/ContentWrapper.vue'
 
 import days from '@/src/util/days'
+import { format } from 'date-fns'
 import { afterschool } from '@/src/api/afterschool'
 
 export default {
   name: 'ManageAfterschool',
   components: { ContentWrapper },
+
+  filters: {
+    filterDate: time => format(time, 'YYYY년 MM월 DD일 HH시 mm분 ss초')
+  },
 
   data () {
     return {
@@ -491,6 +496,12 @@ export default {
             {{ i }}타임
           </dimi-checkbox>
           <span class="mng-afsc__helper">(연강일 경우 두 타임 모두 체크하세요.)</span>
+        </div>
+        <div class="modal__field">
+          <label class="modal__label">
+            기존 신청 기간
+            {{ editModal.afsc.startDate | filterDate }} ~ {{ editModal.afsc.endDate | filterDate }}
+          </label>
         </div>
         <div class="modal__field">
           <label class="modal__label">
